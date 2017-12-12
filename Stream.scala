82ff1b5f5ea58e782cd1032a4f258e3e26dc8bee
Noel Welsh <noel@untyped.com>
2017-12-12 17:03:13 +0000

    Implement `zip` and add dependency on Cats

--- Stream.scala
+++ Stream.scala
@@ -1,7 +1,13 @@
 package stream
 
+import cats.instances.option._
+import cats.syntax.cartesian._
+
 sealed trait Stream[A] {
   import Stream._
+
+  def zip[B](that: Stream[B]): Stream[(A,B)] =
+    Zip(this, that)
 
   def map[B](f: A => B): Stream[B] =
     Map(this, f)
@@ -9,6 +15,8 @@ sealed trait Stream[A] {
   def foldLeft[B](z: B)(f: (B, A) => B): B = {
     def next[C](stream: Stream[C]): Option[C] =
       stream match {
+        case Zip(l, r) =>
+          (next(l) |@| next(r)).tupled
         case Map(s, fun) => next(s).map(fun)
         case FromIterator(s) =>
           if(s.hasNext) Some(s.next()) else None
@@ -22,6 +30,7 @@ sealed trait Stream[A] {
 }
 object Stream {
   // Stream algebraic data type
+  final case class Zip[A,B](left: Stream[A], right: Stream[B]) extends Stream[(A,B)]
   final case class Map[A,B](source: Stream[A], f: A => B) extends Stream[B]
   final case class FromIterator[A](source: Iterator[A]) extends Stream[A]
 
--- build.sbt
+++ build.sbt
@@ -1,1 +1,5 @@
 scalaVersion := "2.12.4"
+
+libraryDependencies += "org.typelevel" %% "cats-core" % "1.0.0-RC1"
+
+scalacOptions += "-Ypartial-unification"
